1bb2f31ecb9dff10e29a37439179736a74d952b2
codersparks <codersparks@gmail.com>
2017-01-02 20:59:09 +0000

    * Reverted back to MongoDB

--- brickorganiser-repository/src/main/java/com/github/codersparks/brickorganiser/repository/PartRepository.java
+++ brickorganiser-repository/src/main/java/com/github/codersparks/brickorganiser/repository/PartRepository.java
@@ -1,14 +1,14 @@
 package com.github.codersparks.brickorganiser.repository;
 
 import com.github.codersparks.brickorganiser.model.Part;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Iterator;
 
 /**
  * Created by codersparks on 27/12/2016.
  */
-public interface PartRepository extends CrudRepository<Part, String> {
+public interface PartRepository extends MongoRepository<Part, String> {
 
     Iterator<Part> findByCategory(int category);
 }
