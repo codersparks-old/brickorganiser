836c0fef771409cfd0ba00d0c3342815b9771e42
codersparks <codersparks@gmail.com>
2017-01-02 21:45:54 +0000

    * Updated DatabaseLoader

--- brickorganiser-repository/src/main/java/com/github/codersparks/brickorganiser/DataBaseLoader.java
+++ brickorganiser-repository/src/main/java/com/github/codersparks/brickorganiser/DataBaseLoader.java
@@ -1,11 +1,14 @@
 package com.github.codersparks.brickorganiser;
 
 import com.github.codersparks.brickorganiser.model.Part;
+import com.github.codersparks.brickorganiser.model.bricklink.BrickLinkCategory;
 import com.github.codersparks.brickorganiser.repository.PartRepository;
+import com.github.codersparks.brickorganiser.repository.bricklink.BrickLinkCategoryRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 /**
@@ -19,12 +22,19 @@ public class DataBaseLoader implements CommandLineRunner{
     @Autowired
     private PartRepository partRepository;
 
+    @Autowired
+    private BrickLinkCategoryRepository brickLinkCategoryRepository;
+
 
     @Override
     public void run(String... strings) throws Exception {
 
+        logger.info("Adding category to database");
+        BrickLinkCategory brickLinkCategory = new BrickLinkCategory(Integer.MAX_VALUE, "testCategory");
+        brickLinkCategoryRepository.save(brickLinkCategory);
+
         logger.info("Adding test part to DB");
-        Part part = new Part("0000", "testItem", 99999999, null, null, null);
+        Part part = new Part("0000", "testItem", "testCategory", null, null, null);
         partRepository.save(part);
     }
 }
